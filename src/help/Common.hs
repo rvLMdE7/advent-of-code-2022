e9b3a1dc5ed4e7fdd5d0b94630552e3ab13ebdd6
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2022-12-01 22:28:27 +0000

    formatting

--- src/help/Common.hs
+++ src/help/Common.hs
@@ -10,13 +10,13 @@ module Common
     , (<<$>>)
     ) where
 
+import Control.Arrow ((>>>))
 import Control.Monad ((>=>))
 import Data.ByteString qualified as Byte
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.Encoding qualified as Text.Enc
 import Data.Void (Void)
-import Flow ((.>))
 import Text.Megaparsec (Parsec)
 import Text.Read (readMaybe)
 
@@ -26,16 +26,16 @@ import Paths_adventofcode2022 (getDataFileName)
 type Parser = Parsec Void Text
 
 readFileUtf8 :: FilePath -> IO Text
-readFileUtf8 = Byte.readFile .> fmap Text.Enc.decodeUtf8
+readFileUtf8 = Byte.readFile >>> fmap Text.Enc.decodeUtf8
 
 readInputFileUtf8 :: FilePath -> IO Text
 readInputFileUtf8 = getDataFileName >=> readFileUtf8
 
 show' :: Show a => a -> Text
-show' = show .> Text.pack
+show' = show >>> Text.pack
 
 readMaybe' :: Read a => Text -> Maybe a
-readMaybe' = Text.unpack .> readMaybe
+readMaybe' = Text.unpack >>> readMaybe
 
 -- | Same as '<$>' but works inside two levels of functors.
 (<<$>>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
--- src/main/Day01.hs
+++ src/main/Day01.hs
@@ -1,10 +1,10 @@
 module Day01 where
 
+import Control.Arrow ((>>>))
 import Data.Ord (Down(Down), comparing)
 import Data.Vector (Vector)
 import Data.Vector qualified as Vec
 import Data.Vector.Algorithms.Merge qualified as Vec.Merge
-import Flow ((.>))
 import System.Exit (die)
 import Text.Megaparsec qualified as Parse
 import Text.Megaparsec.Char qualified as Parse.Char
@@ -14,21 +14,19 @@ import Common (Parser, readInputFileUtf8)
 
 
 parseGroup :: Parser (Vector Word)
-parseGroup = fmap Vec.fromList $
-    Parse.Lex.decimal `Parse.sepEndBy` Parse.Char.eol
+parseGroup = Vec.fromList <$> Parse.sepEndBy Parse.Lex.decimal Parse.Char.eol
 
 parseInput :: Parser (Vector (Vector Word))
-parseInput = fmap Vec.fromList $
-    parseGroup `Parse.sepEndBy` Parse.Char.eol
+parseInput = Vec.fromList <$> Parse.sepEndBy parseGroup Parse.Char.eol
 
 part1 :: Vector (Vector Word) -> Word
-part1 = fmap Vec.sum .> Vec.foldl' max 0
+part1 = fmap Vec.sum >>> Vec.foldl' max 0
 
 part2 :: Vector (Vector Word) -> Word
 part2 = fmap Vec.sum
-    .> Vec.modify (Vec.Merge.sortBy $ comparing Down)
-    .> Vec.take 3
-    .> Vec.sum
+    >>> Vec.modify (Vec.Merge.sortBy $ comparing Down)
+    >>> Vec.take 3
+    >>> Vec.sum
 
 main :: IO ()
 main = do
